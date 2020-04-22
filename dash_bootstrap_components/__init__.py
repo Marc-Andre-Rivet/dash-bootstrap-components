405db54bf5209ed2340f07bb86414b76a258ff43
Marc-André Rivet <marcandre.rivet@sociablelabs.com>
2020-04-22 09:59:56 -0400

    Build for DashR

--- dash_bootstrap_components/__init__.py
+++ dash_bootstrap_components/__init__.py
@@ -3,9 +3,9 @@ import sys
 
 from . import themes  # noqa
 from . import _components
-from ._components import *  # noqa
+from ._imports_ import __all__
 from ._version import __version__  # noqa
-from .table import _generate_table_from_df
+from ._table import _generate_table_from_df
 
 _current_path = os.path.dirname(os.path.abspath(__file__))
 
@@ -21,12 +21,6 @@ _js_dist = [
 ]
 
 _css_dist = []
-
-
-for _component_name in _components.__all__:
-    _component = getattr(_components, _component_name)
-    _component._js_dist = _js_dist
-    _component._css_dist = _css_dist
 
 
 sys.modules[__name__].Table.from_dataframe = classmethod(
